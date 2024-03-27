84a0ffa3a99722718905cc30b043171587032d16
dt0wns <dennis@dennistowns.com>
2024-03-27 11:19:55 -0400

    fix axios variables

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
+    baseURL: process.env.REACT_APP_NODE_ENV === 'development' ? process.env.REACT_APP_API_URL : process.env.REACT_APP_PROD_API_URL,
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
